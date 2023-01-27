6e907652363082f0626d2a446d745b0271476c9a
gschwall <gs682419@ohio.edu>
2023-01-27 15:16:13 -0500

    Code for lab2)

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Garytt Schwall
+ *        Date: 1/27/23
+ * Description: Lab 2 code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
